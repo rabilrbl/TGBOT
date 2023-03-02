64616fe634c377440e8b3b8c93cbf165b3b84b2c
Mohammed Rabil <63334479+rabilrbl@users.noreply.github.com>
2023-03-02 01:51:51 +0000

    Add pyrogram required vars for initilization

--- main.py
+++ main.py
@@ -12,6 +12,8 @@ from revChatGPT.V1 import AsyncChatbot as GPTChatbot
 app = Client(
     "TGBOT",
     bot_token=os.environ.get("BOT_TOKEN"),
+    api_id=os.environ.get("API_ID"),
+    api_hash=os.environ.get("API_HASH"),
 )
 
 CHAT_MODE = "gpt"
